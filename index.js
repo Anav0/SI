0fe1fe0bcee9e6eb733b12e652f02388830691d2
Igor Motyka <igor_motyka@mail.com>
2020-01-10 11:08:22 +0100

    Fixed display

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const QueenSolver = require('./QueenSolver');
 const solver = new QueenSolver();
+
+console.log("Zadanie 2");
+console.log();
+const validPermutations = solver.getAllValidPermutations();
+console.log();
+solver.printPermutationOnBoard(validPermutations[0]);
+console.log();
+solver.printPermutationOnBoard(validPermutations[1]);
+console.log();
+
+console.log("Zadanie 3");
+console.log();
 const permutation = solver.getAllValidPermutationsDFS();
 console.log(permutation)
 solver.printPermutationOnBoard(permutation);
-// const validPermutations = solver.getAllValidPermutations();
-// console.log(validPermutations);
-// solver.printPermutationOnBoard(validPermutations[0]);
-// console.log();
-// solver.printPermutationOnBoard(validPermutations[1]);
+console.log();
